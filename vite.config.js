cec570aebba707d27c06d058373bec311841cb20
Sourav Chauhan <dev.chauhan.sourav@gmail.com>
2025-05-31 22:53:04 +0530

    Add GitHub Pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// Replace 'vue-todo-app' with your repo name
 export default defineConfig({
-  plugins: [vue()],
+  base: '/vue-todo-app/',
+  plugins: [vue()]
 })
